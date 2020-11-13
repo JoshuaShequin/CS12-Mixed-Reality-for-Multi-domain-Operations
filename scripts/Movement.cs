c46cc5ccfd34586eade31a2461100e7a6d92001a
Brenden Smith <brendenmsmith@hotmail.com>
2020-11-12 16:26:17 -0800

    Basic character controller with 3 different views.
    The player prefab includes movement, collision, and jumping. A first-person and 3rd-person camera are part of the prefab and already assigned. The Rotation-Lock Camera will provide a top-down view centered on the player.
    
    The top-down camera will have to be assigned to the player prefab as an available camera, and must not be a child of the player prefab.
    
    Rotation was wonky, so that will be addressed later.

---
+++ scripts/Movement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool grounded;
+    public float playerSpeed = 2.0f;
+    public float jumpHeight = 1.0f;
+    private float gravityValue = -9.81f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Character fall
+        grounded = controller.isGrounded;
+        if (grounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        /*
+        //Mouse rotation
+        Vector2 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
+        Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        float angle = Mathf.Atan2(positionOnScreen.y - mouseOnScreen.y, positionOnScreen.x - mouseOnScreen.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection *= playerSpeed;
+        controller.Move(moveDirection * Time.deltaTime);
+        //Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        /* controller.Move(move * Time.deltaTime * playerSpeed);
+         */
+
+        // Gets move input
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+         {
+             gameObject.transform.forward = move;
+         }
+
+         // Changes player.y to jump
+         if (Input.GetButtonDown("Jump") && grounded)
+         {
+             playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+         }
+
+         playerVelocity.y += gravityValue * Time.deltaTime;
+         controller.Move(playerVelocity * Time.deltaTime);
+
+    }
+}
---
+++ scripts/cameraSwitch.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraSwitch : MonoBehaviour
+{
+    public Camera cam1;
+    public Camera cam2;
+    public Camera cam3;
+    private int currentCam;
+     // Start is called before the first frame update
+    void Start()
+    {
+        cam1.enabled = true;
+        cam2.enabled = false;
+        cam3.enabled = false;
+        currentCam = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Return))
+        {
+            currentCam++;
+            switch(currentCam)
+            {
+                case 2:                 // 2nd camera activated
+                    cam1.enabled = false;
+                    cam2.enabled = true;
+                    cam3.enabled = false;
+                    break;
+                case 3:                 // 3rd camera activated
+                    cam1.enabled = false;
+                    cam2.enabled = false;
+                    cam3.enabled = true;
+                    break;
+                default:                // Reset flag and activate 1st
+                    cam1.enabled = true;
+                    cam2.enabled = false;
+                    cam3.enabled = false;
+                    currentCam = 1;
+                    break;
+            }            
+        }
+        
+    }
+}
