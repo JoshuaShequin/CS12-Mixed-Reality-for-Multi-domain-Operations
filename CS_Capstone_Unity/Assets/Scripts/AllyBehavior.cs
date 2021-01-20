30b50fa72ebed1b41a16ba2378fda7230988fe25
Brenden Smith <35253177+smitbre2@users.noreply.github.com>
2021-01-19 23:09:56 -0800

    Base enemy/ally code
    EnemyBehavior.cs is still failing to detect vision collision of allied forces.
    
    Both scripts allow movement of units on left/right mouse click.

---
+++ CS_Capstone_Unity/Assets/Scripts/AllyBehavior.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+
+public class AllyBehavior : MonoBehaviour
+{
+
+    NavMeshAgent agent;     // Baked pathing agent
+
+    void Start()
+    {
+        // Get unit specific Nav Agent
+        agent = GetComponent<NavMeshAgent>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Will have to get different input key for VR input
+        if (Input.GetMouseButtonDown(0)) {
+            MoveToDest();
+        }
+
+        
+    }
+
+    // Will be called on mouse click to set a move destination
+    void MoveToDest()
+    {
+        RaycastHit hit;
+
+            // This cast input key will have to be matched to VR input
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                agent.destination = hit.point;
+            }
+    }
+
+}
