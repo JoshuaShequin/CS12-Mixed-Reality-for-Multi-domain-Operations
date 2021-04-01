73ea0801b38e422de27ccc96804c05c4e6d2569e
JoshuaShequin <36478500+JoshuaShequin@users.noreply.github.com>
2021-04-01 08:44:55 -0700

    Adding basic healthbars
    Adding basic healthbars to both the enemy and ally NPCs

---
+++ CS_Capstone_Unity/Assets/Scripts/HealthBarEnemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarEnemy : MonoBehaviour
+{
+
+    public GameObject ally;
+    public Image foregroundImage;
+    EnemyBehavior ab;
+
+    int hp_amount;
+    int max_hp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ab = ally.GetComponent<EnemyBehavior>();
+        hp_amount = ab.health;
+        max_hp = hp_amount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hp_amount = ab.health;
+        if (hp_amount == 0)
+        {
+            foregroundImage.fillAmount = 0.0f;
+        }
+        else
+        {
+            foregroundImage.fillAmount = (float)hp_amount / (float)max_hp;
+
+        }
+
+
+    }
+}
