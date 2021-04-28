2d944a0f4cb962c57767dd19e7c498289087573f
JohnathanWebb <37317811+JohnathanWebb@users.noreply.github.com>
2021-04-28 12:07:34 -0700

    Implemented HUD and Pause menu canvas
    Pause menu canvas implemented with buttons but still lacks functionality. HUD in place but only filled with dummy data at this point still requires work on script backend to properly populate the scroll views.

---
+++ CS_Capstone_Unity/Assets/ActiveUnitsDisplay.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ActiveUnitsDisplay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject[] gameObjects;
+        gameObjects = GameObject.FindGameObjectsWithTag("Ally_NPC");
+   
+
+        if(gameObjects.Length == 0) {
+            Debug.Log("No objects tagged with 'Ally_NPC'");
+        } else {
+            Debug.Log("Number of objects found with the tag 'Ally_NPC': " + gameObjects.Length);
+        }
+
+        foreach (GameObject Ally in gameObjects) {
+            AllyBehavior ab = Ally.GetComponent<AllyBehavior>();
+            Debug.Log("Ally Health: " + ab.health);
+        }
+
+    
+
+        for(int i = 0; i < gameObjects.Length; i++) {
+            AllyBehavior ab = gameObjects[i].GetComponent<AllyBehavior>();
+            Debug.Log("Ally Health: " + ab.health);
+            
+        }
+
+
+    }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+
+}
