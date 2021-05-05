fd7ab47717ae6363552eddc76bfa62e8cf9ec783
JohnathanWebb <37317811+JohnathanWebb@users.noreply.github.com>
2021-05-05 12:28:01 -0700

    Revert "Revert "Merge branch 'main' into VR-Controls""
    This reverts commit 98d1c64cf940fa44fb0d11e0399e3cbceca44cbd.

---
+++ CS_Capstone_Unity/Assets/FogOfWar/FogOfWarScript.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogOfWarScript : MonoBehaviour
+{
+
+    public GameObject m_fogOfWarPlane;
+    public string AllyUnitName;
+    public string EnemyUnitName;
+    public GameObject[] units;
+    public GameObject[] EnemyUnits;
+    public LayerMask m_fogLayer;
+    public float m_radius = 5f;
+    private float m_radiusSqr {  get { return m_radius * m_radius; } }
+
+    private Mesh m_mesh;
+    private Vector3[] m_vertices;
+    private Color[] m_colors;
+
+    private bool active = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Initialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        units = GameObject.FindGameObjectsWithTag(AllyUnitName);
+        EnemyUnits = GameObject.FindGameObjectsWithTag(EnemyUnitName);
+        foreach (GameObject Ally in units)
+        {
+            Ray r = new Ray(transform.position, Ally.transform.position - transform.position);
+            RaycastHit hit;
+            
+            if (Physics.Raycast(r, out hit, 1000, m_fogLayer, QueryTriggerInteraction.Collide))
+            {
+                //Debug.Log("length of vertices");
+                //Debug.Log(m_vertices.Length);
+                for (int i = 0; i < m_vertices.Length; i++)
+                {
+                    
+                    Vector3 v = m_fogOfWarPlane.transform.TransformPoint(m_vertices[i]);
+                    //Debug.Log("v");
+                    //Debug.Log(v);
+                    //Debug.Log("hit point");
+                    //Debug.Log(hit.point);
+                    float dist = Vector3.SqrMagnitude(v - hit.point);
+                    //Debug.Log("DIST");
+                    //Debug.Log(dist);
+                    if (dist < m_radiusSqr)
+                    {
+                        
+                        float alpha = Mathf.Min(m_colors[i].a, dist / m_radiusSqr);
+                        m_colors[i].a = alpha;
+                        
+                    }
+                }
+                UpdateColor();
+            }
+        }
+        
+        foreach (GameObject Enemy in EnemyUnits)
+        {
+            EnemyBehavior eb = Enemy.GetComponent<EnemyBehavior>();
+            // Debug.Log(eb);
+            if (eb.state == EnemyBehavior.STATE.ATTACKING)
+            {
+                Ray r = new Ray(transform.position, Enemy.transform.position - transform.position);
+                RaycastHit hit;
+
+                if (Physics.Raycast(r, out hit, 1000, m_fogLayer, QueryTriggerInteraction.Collide))
+                {
+                    //Debug.Log("length of vertices");
+                    //Debug.Log(m_vertices.Length);
+                    for (int i = 0; i < m_vertices.Length; i++)
+                    {
+
+                        Vector3 v = m_fogOfWarPlane.transform.TransformPoint(m_vertices[i]);
+                        //Debug.Log("v");
+                        //Debug.Log(v);
+                        //Debug.Log("hit point");
+                        //Debug.Log(hit.point);
+                        float dist = Vector3.SqrMagnitude(v - hit.point);
+                        //Debug.Log("DIST");
+                        //Debug.Log(dist);
+                        if (dist < m_radiusSqr)
+                        {
+
+                            float alpha = Mathf.Min(m_colors[i].a, dist / m_radiusSqr);
+                            m_colors[i].a = alpha;
+
+                        }
+                    }
+                    UpdateColor();
+                }
+            }
+            
+        }
+        
+
+
+    }
+
+    void Initialize()
+    {
+        m_mesh = m_fogOfWarPlane.GetComponent<MeshFilter>().mesh;
+        m_vertices = m_mesh.vertices;
+        m_colors = new Color[m_vertices.Length];
+        for (int i=0; i < m_colors.Length; i++)
+        {
+            m_colors[i] = Color.black;
+        }
+        UpdateColor();
+    }
+
+    void UpdateColor()
+    {
+        m_mesh.colors = m_colors;
+    }
+
+    public void toggleFogOfWar()
+    {
+        active = !active;
+        m_fogOfWarPlane.SetActive(active);
+    }
+}
