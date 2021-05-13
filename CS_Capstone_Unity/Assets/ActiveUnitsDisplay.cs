eb49466007f5ce6aa10bf2ad2429179d2d2459ff
JoshuaShequin <36478500+JoshuaShequin@users.noreply.github.com>
2021-05-13 13:08:58 -0700

    Adding the active unit view feature for VR
    Adding the active unit view feature for VR

--- CS_Capstone_Unity/Assets/ActiveUnitsDisplay.cs
+++ CS_Capstone_Unity/Assets/ActiveUnitsDisplay.cs
@@ -8,40 +8,35 @@ public class ActiveUnitsDisplay : MonoBehaviour
     // Start is called before the first frame update
     public GameObject UnitLogView;
     public GameObject ActiveUnitView;
+
+    public GameObject text_prefab;
+
+    private GameObject[] allies;
+
+    private bool units_selected = false;
     void Start()
     {
-        GameObject[] gameObjects;
-        gameObjects = GameObject.FindGameObjectsWithTag("Ally_NPC");
-
-      
-   
-
-        if(gameObjects.Length == 0) {
-            Debug.Log("No objects tagged with 'Ally_NPC'");
-        } else {
-            Debug.Log("Number of objects found with the tag 'Ally_NPC': " + gameObjects.Length);
-        }
-
-        foreach (GameObject Ally in gameObjects) {
-            AllyBehavior ab = Ally.GetComponent<AllyBehavior>();
-            Debug.Log("Ally Health: " + ab.health);
-        }
-
-    
-
-        for(int i = 0; i < gameObjects.Length; i++) {
-            AllyBehavior ab = gameObjects[i].GetComponent<AllyBehavior>();
-            Debug.Log("Ally Health: " + ab.health);
-            
-        }
 
 
     }
 
     // // Update is called once per frame
-    // void Update()
-    // {
-        
-    // }
+    void Update()
+    {
+        if (!units_selected)
+        {
+            allies = GameObject.FindGameObjectsWithTag("Ally_NPC");
+            if (allies.Length > 0)
+            {
+                units_selected = true;
+                foreach(GameObject ally in allies)
+                {
+                    var item = Instantiate<GameObject>(text_prefab, ActiveUnitView.transform.Find("Viewport").transform.Find("Content"));
+                    item.GetComponent<UnitStatusUpdater>().ally_NPC = ally;
+                }
+            }
+
+        }
+    }
 
 }
